9af8f2fcc287aa0b679bb43891dcdf16f1d0d5c8
YafteBuenoL <134566047+YafteBuenoL@users.noreply.github.com>
2023-05-28 00:27:14 +0000

    se escriben comentarios

--- imc.py
+++ imc.py
@@ -1,10 +1,10 @@
-print("Hola calcularemos tu IMC")
-nombre = input("Escribe tu nombre: ")
-apellido1 = input("Escribe tu apellido paterno: ")
-apellido2= input("Escribe tu apellido materno: ")
-edad = int(input("Escriba su edad: "))
-estatura = float(input("Escriba su estatura: "))
-peso = float(input("Escriba su peso: "))
+print("Hola calcularemos tu IMC")#se da la bienvenida
+nombre = input("Escribe tu nombre: ")#se pide escribir nombre
+apellido1 = input("Escribe tu apellido paterno: ")#se pide escribir apellido paterno
+apellido2= input("Escribe tu apellido materno: ")#se pide escribir apellido materno
+edad = int(input("Escriba su edad: "))#se pide escribir la edad
+estatura = float(input("Escriba su estatura: "))#se pide escribir la estatura
+peso = float(input("Escriba su peso: "))#se pide escribir peso
 
 
 print ("Sus datos son: ")
@@ -16,7 +16,8 @@ print (f"Su estatura es: {estatura}")
 print (f"Su peso es: {peso}")
 print ("Su Indice de Masa Corporal es: ")
 
-a = (estatura**2)
+a = (estatura ** 2)
 imc = (peso / a)
 
+round(imc)
 print(imc)
