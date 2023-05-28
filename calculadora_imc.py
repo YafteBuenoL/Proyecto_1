1f64b32877b5978025a15ebffca1c5b06648021c
YafteBuenoL <134566047+YafteBuenoL@users.noreply.github.com>
2023-05-28 03:30:41 +0000

    cambio2

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,13 +1,11 @@
-
-# calculadora de IMC
+#calculadora de IMC
 
 print ("Hola calcularemos tu IMC")#se da la bienvenida
 nombre = input("Escribe tu nombre: ")#se pide escribir nombre
 apellido1 = input("Escribe tu apellido paterno: ")#se pide escribir apellido paterno
 apellido2= input("Escribe tu apellido materno: ")#se pide escribir apellido materno
-
 edad = int(input("Escriba su edad: "))#se pide escribir la edad
-estatura = float(input("Escriba su estatura en cm: "))#se pide escribir la estatura
+estatura = float(input("Escriba su estatura en metros: "))#se pide escribir la estatura
 peso = float(input("Escriba su peso en kg: "))#se pide escribir peso
 
 
@@ -16,9 +14,9 @@ print ("Sus datos son: ")
 print (f"Nombre: {nombre}")
 print (f"Apellido paterno: {apellido1}")
 print (f"Apellido materno: {apellido2}")
-print (f"Su edad es: {edad}")
-print (f"Su estatura es: {estatura}")
-print (f"Su peso es: {peso}")
+print (f"Su edad es: {edad} " " años")
+print (f"Su estatura es: {estatura} " " metros")
+print (f"Su peso es: {peso} " "kilogramos ")
 
 imc = round(peso/(estatura ** 2),2) #se imprime el valor de imc
 
