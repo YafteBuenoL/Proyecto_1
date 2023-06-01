297660fecc263a1d064a32714c27153338c3446a
YafteBuenoL <134566047+YafteBuenoL@users.noreply.github.com>
2023-06-01 19:06:50 +0000

    se ponen separaciones

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -8,7 +8,7 @@ apellido2= input("Escribe tu apellido materno: ")#se pide escribir apellido mate
 edad = int(input("Escriba su edad: "))#se pide escribir la edad
 estatura = float(input("Escriba su estatura en metros: "))#se pide escribir la estatura
 peso = float(input("Escriba su peso en kg: "))#se pide escribir peso
-
+print("-------------------------------")
 
 #Se imprimen los datos
 print ("SUS DATOS SON: ")
@@ -18,6 +18,7 @@ print (f"Apellido materno: {apellido2}")
 print (f"Su edad es: {edad} " " años")
 print (f"Su estatura es: {estatura} " " metros")
 print (f"Su peso es: {peso} " "kilogramos ")
+print("--------------------------------")
 
 imc = round(peso/(estatura ** 2),2) #se imprime el valor de imc
 
@@ -27,6 +28,8 @@ if edad > 18:
 else:
     print("Usted aun no tiene la mayoria de edad")
 print ("Su indice de masa corporal es de:",imc)
+
+print("---------------------------------")
 
 #Aqui se identifica que tipo que tipo de condicion tiene la persona dependiendo del valor de imc
 if imc <= 18.9:
